e32e187fbe7e6e1d65bb3c04bf439a5e598885ff
Adeline Simon <adeline.simon31@hotmail.fr>
2020-03-31 23:38:26 +0200

    Updates

---
+++ precache-manifest.b9dcf57029d7f1fcf1123a71d845c1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ae8b9a88e3f635c6d3c831ca10c9a8",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "120453c86f90e64032c8",
+    "url": "/cv/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a9ff056ffb97dbf13e5d",
+    "url": "/cv/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "120453c86f90e64032c8",
+    "url": "/cv/static/js/2.beba3cc4.chunk.js"
+  },
+  {
+    "revision": "14a031df65522d808a3f88f5c2a0903f",
+    "url": "/cv/static/js/2.beba3cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ff056ffb97dbf13e5d",
+    "url": "/cv/static/js/main.cd2e5321.chunk.js"
+  },
+  {
+    "revision": "c9924dad7afcaec7d4d5",
+    "url": "/cv/static/js/runtime-main.794131ee.js"
+  }
+]);
