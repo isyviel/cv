7069271d5fb966e59e5a9404912aa28706c30c3b
Adeline Simon <adeline.simon31@hotmail.fr>
2020-04-03 00:43:43 +0200

    logos

--- src/component/content/TechSec.js
+++ src/component/content/TechSec.js
@@ -5,21 +5,21 @@ import {Col} from "@bootstrap-styled/v4/lib"
 const CustomImg = styled.img`
    
    margin-top: 5px;
-   height: 80px;
+   height: 60px;
 
    @media screen and (max-width:768px) {
-      height: 60px;   
+      height: 50px;   
    }
 `
 
-const Tech = ({tech, ...props}) => {
+const TechSec = ({techSec, ...props}) => {
    const path = "images/";
       return (
       <Col xs="auto text-align-center">
-         <CustomImg src= {path + tech[1]}/>
+         <CustomImg src= {path + techSec[1]}/>
       </Col>
       )
       
 }
 
-export default Tech
+export default TechSec
