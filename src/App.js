8f476c98d06549280740c0794fd06e16c3b76d53
Adeline Simon <adeline.simon31@hotmail.fr>
2020-04-02 22:10:59 +0200

    no prallaxe

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import OnePage from './component/OnePage'
 import React from "react"
 import { HashRouter as Router, Route} from "react-router-dom"
-import { ParallaxProvider } from 'react-scroll-parallax';
-
 
 function App() {
   return (
