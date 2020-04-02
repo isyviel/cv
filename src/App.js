25fa61b77e698a68d57a27db8e866a54bed811f4
Adeline Simon <adeline.simon31@hotmail.fr>
2020-04-02 19:18:20 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import OnePage from './component/OnePage'
 import React from "react"
-import { BrowserRouter as Router, Route} from "react-router-dom"
+import { HashRouter as Router, Route} from "react-router-dom"
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 
