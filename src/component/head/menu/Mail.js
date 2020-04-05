e01d5c0eb9b967ad2ef1832562da4763679f113a
Adeline Simon <adeline.simon31@hotmail.fr>
2020-04-05 19:07:25 +0200

    remonter les infos du formulaire + affichage loader

--- src/component/head/menu/Mail.js
+++ src/component/head/menu/Mail.js
@@ -1,16 +1,20 @@
 import React , {useState} from "react";
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
 import { ThemeProvider } from "styled-components"
 import ImageButton from "../../common/ImageButton"
 import Them from "../../common/theme/MatThemes";
-import { FormControl } from '@material-ui/core';
+import { FormControl, DialogTitle, LinearProgress, DialogContent, Dialog } from '@material-ui/core';
 import Form from './mail/Form'
+import emailjs from 'emailjs-com'
+import USER from '../../common/constantes/mail'
 
 const Mail = () => {
-    const [open, setOpen] = useState(false);
-  
+    const [open, setOpen] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [values,setValues] = useState({})
+    
+    const user = USER
+
     const handleClickOpen = (event) => {
      setOpen(true)
     };
@@ -18,7 +22,39 @@ const Mail = () => {
     const handleClose = () => {
       setOpen(false)
     };
-    
+
+    const handleChange = (event) => {
+      event.persist();
+      setValues({...values, [event.target.name]: event.target.value});
+    }
+
+    const handleSubmit = (event) => {
+      event && event.preventDefault()
+      
+      const templateParams = {
+        from_name: values.mail,
+        to_name: user,
+        object: values.objet,
+        content: values.message,
+        name: values.nom,
+        firstname: values.prenom,
+        phone: values.phone
+      }
+      setIsLoading(true)
+      emailjs.init(user);
+      emailjs.send('outlook', 'contact', templateParams, user)
+      
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text)
+        alert("Votre e-mail a été envoyé")
+        setIsLoading(false)
+        
+     }, (error) => {
+        console.log('FAILED...', error);
+        alert("Veuillez vérifier votre saisie ou me contacter à cette adresse : adeline.simon31@hotmail.fr")
+        setIsLoading(false)
+     });
+    }
 
     return (
       <div>
@@ -27,9 +63,15 @@ const Mail = () => {
         </Button>
         <ThemeProvider theme={Them}>
           <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" fullWidth>
+              <DialogTitle>Pour me contacter...</DialogTitle>
+              {isLoading && (<LinearProgress color="secondary"/>)}
               <DialogContent className="p-2">
                 <FormControl>
-                  <Form windowClose={handleClose}/>
+                  <Form 
+                  windowClose={handleClose} 
+                  submit={handleSubmit}
+                  values={values}
+                  change={handleChange}/>
                 </FormControl>
               </DialogContent>
           </Dialog>
--- src/component/head/menu/mail/Form.js
+++ src/component/head/menu/mail/Form.js
@@ -1,58 +1,21 @@
-import React, {useState} from "react"
+import React from "react"
 import Input from './Input'
 import Area from './Area'
 import {Row} from "@bootstrap-styled/v4/lib"
 import {Container} from "@bootstrap-styled/v4"
 import Button from '@material-ui/core/Button';
-import emailjs from 'emailjs-com'
-import USER from '../../../common/constantes/mail'
 
-const Form = ({title,content, windowClose, send,change,value,error, response,...props}) => { 
-
-  const [values,setValues] = useState({})
-  const user = USER
-
-  const handleSubmit = (event) => {
-      event && event.preventDefault()
-      
-      const templateParams = {
-        from_name: values.mail,
-        to_name: user,
-        object: values.objet,
-        content: values.message,
-        name: values.nom,
-        firstname: values.prenom,
-        phone: values.phone
-      }
-      alert("Envoi en cours....")
-      emailjs.init(user);
-      emailjs.send( 'outlook', 'contact', templateParams, user)
-      
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-        alert("Votre mail a bien été envoyé")
-        
-     }, (error) => {
-        console.log('FAILED...', error);
-        alert("Veuillez vérifier votre saisie ou me contacter à cette adresse : adeline.simon31@hotmail.fr")
-     });
-    }
-
-  const handleChange = (event) => {
-    event.persist();
-    setValues({...values, [event.target.name]: event.target.value});
-  }
+const Form = ({content, windowClose, send,error, response,submit,values,change,...props}) => { 
 
     return (
         <Container>
-          <p>Pour me contacter :</p>
-          <form onSubmit={handleSubmit}>
-            <Input required id="standard-required standard-error-helper-text" label="E-Mail" name="mail" value={values.mail} change={handleChange}/>
-            <Input label="Objet : " name="objet" value={values.objet} change={handleChange} required={false}/>
-            <Area label="Votre message" name="message" value={values.message} change={handleChange}/>
-            <Input label="Nom" name="nom" value={values.nom} required={false} change={handleChange}/>
-            <Input label="Prénom" name="prenom" value={values.prenom} required={false} change={handleChange}/>
-            <Input label="Téléphone" name="phone" value={values.phone} change={handleChange} required={false}/>
+          <form onSubmit={submit} values={values}>
+            <Input required id="standard-required standard-error-helper-text" label="E-Mail" name="mail" value={values.mail} change={change}/>
+            <Input label="Objet : " name="objet" value={values.objet} change={change} />
+            <Area label="Votre message" name="message" value={values.message} change={change}/>
+            <Input label="Nom" name="nom" value={values.nom}  change={change}/>
+            <Input label="Prénom" name="prenom" value={values.prenom}  change={change}/>
+            <Input label="Téléphone" name="phone" value={values.phone} change={change} />
             <Row className="justify-content-around">
               <Button type="submit" color="secondary" className="mt-3 mb-3">
                 Envoyer
